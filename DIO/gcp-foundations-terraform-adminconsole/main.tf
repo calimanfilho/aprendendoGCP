6ac054c106a4f0ba2d0c2a6537c8fe080e66621c
Esmael Caliman Filho <calimanfilho@gmail.com>
2022-12-10 19:13:07 -0300

    Adiciona implementação de Terraform para Admin Console

---
+++ DIO/gcp-foundations-terraform-adminconsole/main.tf
@@ -0,0 +1,42 @@
+provider "googleworkspace" {
+  credentials             = "${file("serviceaccount.yaml")}"
+  customer_id             = "informaraqui"
+  impersonated_user_email = "emailaqui"
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/admin.directory.user",
+    "https://www.googleapis.com/auth/admin.directory.userschema",
+    "https://www.googleapis.com/auth/admin.directory.group",	
+    # include scopes as needed
+  ]
+}
+
+resource "googleworkspace_group" "devops" {
+  email       = "devops@minha-empresa.com.br"
+  name        = "DevOps"
+  description = "DevOps Group"
+
+  aliases = ["dev-ops@minha-empresa.com.br"]
+
+  timeouts {
+    create = "1m"
+    update = "1m"
+  }
+}
+
+resource "googleworkspace_user" "joao" {
+  primary_email = "joao@minha-empresa.com.br"
+  password      = "34819d7beeabb9260a5c854bc85b3e44"
+  hash_function = "MD5"
+
+  name {
+    family_name = "João"
+    given_name  = "Ninguém"
+  }
+}
+
+resource "googleworkspace_group_member" "manager" {
+  group_id = googleworkspace_group.devops.id
+  email    = googleworkspace_user.joao.primary_email
+
+  role = "MANAGER"
+}
